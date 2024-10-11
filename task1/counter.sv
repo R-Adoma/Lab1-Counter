368cc79e2e81127e2f3d496c5c1ff054b4436f54
root <root@RKA-Surface>
2024-10-11 10:03:40 +0100

    Modified Counter

--- task1/counter.sv
+++ task1/counter.sv
@@ -1,5 +1,5 @@
 module counter#(
-    parameter WIDTH = 8;
+    parameter WIDTH = 8
 )(
     //Interface Signals
     input logic clk,  //clock
@@ -10,6 +10,6 @@ module counter#(
 
 always_ff @(posedge clk)
     if (rst) count <= {WIDTH{1'b0}};
-    else     count <= count + {{WIDTH-1:{1'b0}}, en};
+    else     count <= count + {{WIDTH-1{1'b0}}, en};
 
 endmodule
